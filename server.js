6513482906a8227c3c773d9de6da4722303664f3
kwolek90 <karolwolek@student.uw.edu.pl>
2026-01-13 13:58:24 +0100

    Lighther main screen

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Route for the main page
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'pipboy.html'));
 });
 
 // API endpoint to list available games
